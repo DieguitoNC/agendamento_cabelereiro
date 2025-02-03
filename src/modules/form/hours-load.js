86e4dd0d0c779e6772ccfb2d48d32ae4b893a6ce
DieguitoNC <diegocatalann@gmail.com>
2025-02-03 10:57:55 -0300

    arrumando_horario_bloqueado

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({date}) {
     const opening = openingHours.map((hour) => {
         //Recupera somente a hora
@@ -9,11 +11,22 @@ export function hoursLoad({date}) {
 
 
         //Adiciona a hora a data e verificar se esta no passado
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
         
-        console.log({
+        return{
             hour,
             available : isHourPast,
-        })
+        }
     })
+
+  // Renderiza os horarios
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour
+    hours.append(li)
+  })
 }
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -10,7 +10,7 @@ export function scheduleDays(){
 
 
     // Renderiza as horas disponiveis
-    hoursLoad({})
+    hoursLoad({date})
 }
 
 
